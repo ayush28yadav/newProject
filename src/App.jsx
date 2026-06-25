c197342e49f5b2e53f5f71079ccf3267333eaea1
ayush28yadav <ayush28yadav@gmail.com>
2026-06-25 15:01:29 +0530

    new push

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,32 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 //import Counter from './components/Counter'
-//import Child1 from './components/child1'
+import Child1 from './components/child1'
 import Cards from './components/Cards'
 
 function App() {
-  const [info, setInfo] = useState([{name: "Ayush",
-    rollno: 21
-  },
-  {name: "Akshay",
-    rollno: 22
-  },{name: "Arit",
-    rollno: 23
-  },{name : "Ankit",
-    rollno: 24
-  }
+//   const [info, setInfo] = useState([{name: "Ayush",
+//     rollno: 21
+//   },
+//   {name: "Akshay",
+//     rollno: 22
+//   },{name: "Arit",
+//     rollno: 23
+//   },{name : "Ankit",
+//     rollno: 24
+//   }
 
-])
+// ])
+const name = "Ayush";
+const rollno = 21;
 
   return (
     <>
+    <Child1 name = {name} rollno = {rollno}/>
      {/* <Child1 name = {info[0].name}  rollno = {info[0].rollno}/> */}
-   {info.map((item, index) => (
+   {/* {info.map((item, index) => (
     <Cards key={index} name = {item.name} age = {item.rollno}/>
-   ))}
+   ))} */}
     </>
   )
 }
